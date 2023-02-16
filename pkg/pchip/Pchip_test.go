b18dc53d8f6c8d5edcbca5d5b6129a71a37c2d85
Roman Blinov <romanblinov2013@yandex.ru>
2023-02-16 18:04:34 +0300

    Edit test Pchip

--- pkg/pchip/Pchip_test.go
+++ pkg/pchip/Pchip_test.go
@@ -1,1 +1,31 @@
 package pchip
+
+import (
+	"main/pkg/oss"
+	"math"
+	"testing"
+)
+
+func TestPchip(t *testing.T) {
+	win := 1024
+	lag := int(math.Floor(float64(win) / 10.0))
+	lgl := make([]float64, lag)
+	for m := 0; m < len(lgl); m++ {
+		lgl[m] = float64(m + 1)
+	}
+
+	PhaAcfNrm, _ := oss.Make_singnal_xn("PhaAcfNrm") // Загрузить сигнал из файла PhaAcfNrm.xlsx
+
+	_, pCoef := Pchip(oss.VecDense_in_float64(PhaAcfNrm),
+		lgl,
+		lgl,
+		len(PhaAcfNrm), len(lgl))
+
+	//fmt.Println(pCoef)
+	oss.SafeToXlsxDualArray(pCoef, "pCoef")
+	/*
+		if result != "Foo" {
+			t.Errorf("Result was incorrect, got: %s, want: %s.", result, "Foo")
+		}
+	*/
+}
