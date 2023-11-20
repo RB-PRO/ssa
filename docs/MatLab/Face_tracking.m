b74cfbf25d60bc43e12fdca920913b29cf67cb68
Roman Blinov <romanblinov2013@yandex.ru>
2023-11-20 07:26:04 +0300

    Видеокомпенсация

--- docs/MatLab/Face_tracking.m
+++ docs/MatLab/Face_tracking.m
@@ -1,6 +1,9 @@
 function RGB = Face_tracking(VideoFile)
     vid_obj = VideoReader(VideoFile);
     VideoFile = NameVideoFile(VideoFile);
+    
+    %
+
 
 
     FDetect = vision.CascadeObjectDetector('FrontalFaceCART','MergeThreshold',10); %Viola-Jones Algorithm
@@ -77,6 +80,12 @@ function RGB = Face_tracking(VideoFile)
 
         % Extract the next video frame
         videoFrame = readFrame(vid_obj);
+        
+            A_lin = rgb2lin(videoFrame);
+    percentiles = 10;
+    illuminant = illumgray(A_lin,percentiles);
+    B_lin = chromadapt(A_lin,illuminant,'ColorSpace','linear-rgb');
+    videoFrame = lin2rgb(B_lin);
          
 %         if frame < 200
 %              continue
